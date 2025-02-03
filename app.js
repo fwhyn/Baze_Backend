abf675663fcdf0b5d94572ec9f40d552bc6c8949
Yana Wahyuna <yana.wahyuna@epson.co.id>
2025-02-03 16:02:52 +0700

    Login Implementation

--- app.js
+++ app.js
@@ -1,3 +1,33 @@
 const express = require('express');
-const mysql = require("mysql")
-const dotenv = require('dotenv')
+const dotenv = require('dotenv');
+const mysql = require('mysql');
+
+// const app = express();
+// dotenv.config({ path: './.env' });
+
+// const db = mysql.createConnection({
+//     host: process.env.DATABASE_HOST,
+//     user: process.env.DATABASE_USER,
+//     password: process.env.DATABASE_PASSWORD,
+//     database: process.env.DATABASE
+// });
+
+// db.connect((error) => {
+//     if (error) {
+//         console.log('Error connecting to MySQL:', error);
+//     } else {
+//         console.log('MySQL connected!');
+//         // Perform a simple query to check the connection
+//         db.query('SELECT 1 + 1 AS solution', (err, results, fields) => {
+//             if (err) {
+//                 console.log('Error executing query:', err);
+//             } else {
+//                 console.log('Query result:', results[0].solution); // Should print 2
+//             }
+//         });
+//     }
+// });
+
+// app.listen(3000, () => {
+//     console.log('Server is running on port 3000');
+// });
