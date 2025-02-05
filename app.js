191493f429b0f50f024565ccba8fab4216baf163
Yana Wahyuna <yana.wahyuna@epson.co.id>
2025-02-05 15:29:40 +0700

    Register and Login

--- app.js
+++ app.js
@@ -2,6 +2,7 @@ const express = require('express');
 const dotenv = require('dotenv');
 const mysql = require('mysql');
 const path = require('path');
+const bcrypt = require('bcryptjs');
 
 const app = express();
 dotenv.config({ path: './.env' });
@@ -32,9 +33,51 @@ db.connect((error) => {
 });
 
 app.set('view engine', 'hbs');
+
 app.use(express.static(publicDir));
+app.use(express.urlencoded({ extended: false }));
+app.use(express.json());
+
 app.get('/', (req, res) => {
     res.render('index');
+});
+app.get("/register", (req, res) => {
+    res.render("register")
+})
+app.get("/login", (req, res) => {
+    res.render("login")
+})
+
+app.post('/register', async (req, res) => {
+    const { name, email, password, password_confirm } = req.body;
+
+    db.query('SELECT email FROM users WHERE email = ?', [email], async (error, result) => {
+        if (error) {
+            console.log(error);
+        }
+
+        if (result.length > 0) {
+            return res.render('register', {
+                message: 'This email is already in use'
+            });
+        } else if (password !== password_confirm) {
+            return res.render('register', {
+                message: 'Passwords do not match!'
+            });
+        }
+
+        let hashedPassword = await bcrypt.hash(password, 8);
+
+        db.query('INSERT INTO users SET ?', { name: name, email: email, password: hashedPassword }, (err, result) => {
+            if (err) {
+                console.log(err);
+            } else {
+                return res.render('register', {
+                    message: 'User registered!'
+                });
+            }
+        });
+    });
 });
 
 app.listen(5000, () => {
